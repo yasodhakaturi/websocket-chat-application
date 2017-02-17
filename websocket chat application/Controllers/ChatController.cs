7fe15363d0ce5848b02bc4c4bf3a70d04a8f2bd3
yasodha <yasodha.bitra@gmail.com>
2017-02-17 14:52:31 -0800

    commiting code

--- websocket chat application/Controllers/ChatController.cs
+++ websocket chat application/Controllers/ChatController.cs
@@ -12,6 +12,12 @@ using System.Threading;
 using System.Threading.Tasks;
 using System.Web.WebSockets;
 using Microsoft.Web.WebSockets;
+using websocket_chat_application.Models;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Core.Objects;
 
 
 namespace websocket_chat_application.Controllers
@@ -58,32 +64,153 @@ namespace websocket_chat_application.Controllers
            HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler(username));
            return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
        }
-     
-       class ChatWebSocketHandler : WebSocketHandler
-       {
-           private static WebSocketCollection _chatClients = new WebSocketCollection();
-           private string _username;
-    
-           public ChatWebSocketHandler(string username)
-          {
-               _username = username;
-           }
-   
-           public override void OnOpen()
-           {
-               _chatClients.Add(this);
-           }
-   
-           public override void OnMessage(string message)
-           {
-              _chatClients.Broadcast(_username + ": " + message);
-           }
-      }
+
+        class ChatWebSocketHandler : WebSocketHandler
+        {
+            shortenURLEntities dc = new shortenURLEntities();
+
+            private static WebSocketCollection _chatClients = new WebSocketCollection();
+            private string _userid;
+
+            //     public ChatWebSocketHandler(string username)
+            //    {
+            //         _username = username;
+            //     }
+
+            //     public override void OnOpen()
+            //     {
+            //         _chatClients.Add(this);
+            //     }
+
+            //     public override void OnMessage(string message)
+            //     {
+            //        _chatClients.Broadcast(_username + ": " + message);
+            //     }
+            //}
+
+            public ChatWebSocketHandler(string userid)
+            {
+                _userid = userid;
+            }
+
+            public override void OnOpen()
+            {
+                _chatClients.Add(this);
+                var res = getdata(_userid);
+                _chatClients.Broadcast("result: " + res);
+            }
+
+            //public override void OnMessage(string message)
+            //{
+            //   _chatClients.Broadcast(_username + ": " + message);
+            //}
+            public class appUrlModel
+            {
+                public string admin { get; set; }
+                public string analytics { get; set; }
+                public string landing { get; set; }
+            }
+
+            public DashBoardSummary getdata(string userid)
+            {
+                int c_id = Convert.ToInt32(userid);
+                DashBoardSummary obj = new DashBoardSummary();
+                SqlConnection lSQLConn = null;
+                SqlCommand lSQLCmd = new SqlCommand();
+                Client obj_client = dc.Clients.Where(x => x.PK_ClientID == c_id).Select(y => y).SingleOrDefault();
+                if (obj_client != null)
+                {
+                    string connStr = ConfigurationManager.ConnectionStrings["shortenURLConnectionString"].ConnectionString;
+
+                    // create and open a connection object
+                    lSQLConn = new SqlConnection(connStr);
+                    SqlDataReader myReader;
+                    lSQLConn.Open();
+                    lSQLCmd.CommandType = CommandType.StoredProcedure;
+                    lSQLCmd.CommandText = "spGetDashBoardSummary";
+                    lSQLCmd.Parameters.Add(new SqlParameter("@FkClientId", c_id));
+                    lSQLCmd.Connection = lSQLConn;
+                    myReader = lSQLCmd.ExecuteReader();
+
+                    totalUrls totalUrls = ((IObjectContextAdapter)dc)
+                      .ObjectContext
+                      .Translate<totalUrls>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    users users = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<users>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    visits visits = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<visits>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    campaigns campaigns = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<campaigns>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    List<recentCampaigns1> recentCampaigns = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<recentCampaigns1>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).ToList();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    today today = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<today>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    last7days last7days = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<last7days>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    // Move to locations result 
+                    myReader.NextResult();
+                    month month = ((IObjectContextAdapter)dc)
+                   .ObjectContext
+                   .Translate<month>(myReader, "SHORTURLDATAs", MergeOption.AppendOnly).SingleOrDefault();
+
+                    List<recentCampaigns> objr = (from r in recentCampaigns
+                                                  select new recentCampaigns()
+                                                  {
+                                                      id = r.id,
+                                                      rid = r.rid,
+                                                      visits = r.visits,
+                                                      users = r.users,
+                                                      status = r.status,
+                                                      //crd = r.createdOn.Value.ToString("MM/dd/yyyyThh:mm:ss")
+                                                      createdOn = r.crd.Value.ToString("yyyy-MM-ddThh:mm:ss"),
+                                                      endDate = (r.endd == null) ? null : (r.endd.Value.ToString("yyyy-MM-ddThh:mm:ss"))
+
+                                                  }).ToList();
+
+                    activities obj_act = new activities();
+                    obj.totalUrls = totalUrls;
+                    obj.users = users;
+                    obj.visits = visits;
+                    obj.campaigns = campaigns;
+                    obj.recentCampaigns = objr;
+                    obj_act.today = today;
+                    obj_act.last7days = last7days;
+                    obj_act.month = month;
+                    obj.activities = obj_act;
+
+                }
+                return obj;
+
+            }
 
 
 
-
-
+        }
          
     }
 }
---
+++ websocket chat application/Models/DashBoardSummary.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace websocket_chat_application.Models
+{
+   
+        public class DashBoardSummary
+        {
+            public totalUrls totalUrls { get; set; }
+            public users users { get; set; }
+            public visits visits { get; set; }
+            public campaigns campaigns { get; set; }
+            public List<recentCampaigns> recentCampaigns { get; set; }
+            public activities activities { get; set; }
+
+        }
+        public class totalUrls
+        {
+            public int count { get; set; }
+        }
+        public class users
+        {
+            public int total { get; set; }
+            public int uniqueUsers { get; set; }
+            public int uniqueUsersToday { get; set; }
+            public int usersToday { get; set; }
+            public int uniqueUsersYesterday { get; set; }
+            public int usersYesterday { get; set; }
+            public int uniqueUsersLast7days { get; set; }
+            public int usersLast7days { get; set; }
+        }
+        public class visits
+        {
+            public int total { get; set; }
+            public int uniqueVisits { get; set; }
+            public int visitsToday { get; set; }
+            public int uniqueVisitsToday { get; set; }
+            public int visitsYesterday { get; set; }
+            public int uniqueVisitsYesterday { get; set; }
+            public int uniqueVisitsLast7days { get; set; }
+            public int visitsLast7days { get; set; }
+        }
+        public class campaigns
+        {
+            public int total { get; set; }
+            public int campaignsLast7days { get; set; }
+            public int campaignsMonth { get; set; }
+
+        }
+        public class recentCampaigns
+        {
+            public Int64 id { get; set; }
+            public string rid { get; set; }
+            public string createdOn { get; set; }
+            public string endDate { get; set; }
+            //public DateTime? crd { get; set; }
+            //public DateTime? endd { get; set; }
+            public int visits { get; set; }
+            public int users { get; set; }
+            public bool status { get; set; }
+        }
+        public class recentCampaigns1
+        {
+            public Int64 id { get; set; }
+            public string rid { get; set; }
+            //public string createdOn { get; set; }
+            //public string endDate { get; set; }
+            public DateTime? crd { get; set; }
+            public DateTime? endd { get; set; }
+            public int visits { get; set; }
+            public int users { get; set; }
+            public bool status { get; set; }
+        }
+        public class activities
+        {
+            public today today { get; set; }
+            public last7days last7days { get; set; }
+            public month month { get; set; }
+        }
+        public class today
+        {
+            public int urlTotal { get; set; }
+            public double urlPercent { get; set; }
+            public int visitsTotal { get; set; }
+            public double visitsPercent { get; set; }
+            public int revisitsTotal { get; set; }
+            public double revisitsPercent { get; set; }
+            public int noVisitsTotal { get; set; }
+            public double noVisitsPercent { get; set; }
+
+        }
+        public class last7days : today
+        {
+            //public string urlTotal { get; set; }
+            //public string urlPercent { get; set; }
+            //public string visitsTotal { get; set; }
+            //public string visitsPercent { get; set; }
+            //public string revisitsTotal { get; set; }
+            //public string revisitsPercent { get; set; }
+            //public string noVisitsTotal { get; set; }
+            //public string noVisitsPercent { get; set; }
+        }
+        public class month : today
+        {
+        }
+    
+}
